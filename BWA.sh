cfdd4d847cc9d3f276a51a1fc36ce02748402d6c
isabelluo <iisabel1234tw@gmail.com>
2022-10-05 16:07:52 -0400

    contig generation

--- BWA.sh
+++ BWA.sh
@@ -5,12 +5,15 @@ refdir='/home/garcialab/BWA_reference'
 source activate ngs_env
 
 # cat *.fasta | bwa index -p catted .commands to create concatenated index out of 16 files
+#faidx catted fasta files to create indexing for mpileup
+
 for i in *.fastq
 do
 	bwa mem -M -t 8 $refdir/cat_US/catted_USregion.fasta $i | samtools sort -o cat_${i}_sort.bam
 	samtools index cat_${i}_sort.bam # you need a index file for it to find the read counts
 done
 	 # converting alignment sam to binary bam, -S specify input sam, -b specify output bam
+
 #sort command will sort the imput reads in genome order
  # going straight to bam should be faster. Works in less than 3 min. I have compared, there is no difference.
 # try this in the future: samtools coverage -r chr1:1M-12M input.bam
@@ -32,4 +35,9 @@ do
 	samtools depth -a $i | awk '{c++;s+=$3}END{print s/c " average depth"}' >> ${i:0:-4}_sams_stat.txt
 	samtools depth -a $i | awk '{c++; if($3>200) total+=1}END{print (total/c)*100 " breadth of 200x coverage"}' >> ${i:0:-4}_sams_stat.txt
 	#samtools mpileup -aa -A -d 10000000 -Q 20 $i  | bcftools consensus -f $refdir/cat_US/catted_USregion.fasta > ${i:0:-4}_consensus.fa
+done
+
+while read -r line
+do
+	samtools mpileup -aa -A -d 10000000 -Q 20 -r $line cat_BC04_ILTV_ReadIDs.fastq_sort.bam | ivar consensus -t .8 -m 20 -p $line
 done
