5cd68854bb999db44cd693f5b3ab9ee39092638a
isabelluo <iisabel1234tw@gmail.com>
2022-10-05 13:13:13 -0400

    change the fast5files variables ``

--- A_fast5_guppy_test.sh
+++ A_fast5_guppy_test.sh
@@ -9,13 +9,13 @@ do
     if [ ! -d $current_directory/fast5/$i ]
     then
         mkdir $current_directory/fast5/$i # copy and paste your path for the current directory
-        fast5files=$(ls $current_directory/fast5/ | grep ".fast5" | wc -l)
-    elif [ $fast5files -ge 2 ] # move the files onece there are more the 2 fast5 files generated 
+        fast5files=`ls $current_directory/fast5/ | grep ".fast5" | wc -l`
+    elif [ $fast5files -ge 2 ] # move the files onece there are more the 2 fast5 files generated
     then
-        mv $current_directory/fast5/*.fast5 $current_directory/fast5/$i/. # copy and paste your path for the current directory 
+        mv $current_directory/fast5/*.fast5 $current_directory/fast5/$i/. # copy and paste your path for the current directory
         guppy_basecaller --input_path $current_directory/fast5/$i --save_path $current_directory/fastq/$i --flowcell FLO-MIN106 --kit SQK-LSK109 --device auto --calib_detect
         # the save path should be in fastq folder # specify your kit and flowcell
-        echo "${i} folder ${fast5files} files " # optional,but it will tell you how many folders is in each files 
+        echo "${i} folder ${fast5files} files " # optional,but it will tell you how many folders is in each files
         ((i++)) # increases i by 1 each time
     elif test -d $current_directory/fastq/$i #try to let it skip to the largest one?
     then
@@ -26,4 +26,4 @@ do
         echo "current basecalling folder ${i}"
     fi
 done
-# what if you turn off the window accidentally, how can you start basecalling again? 
+# what if you turn off the window accidentally, how can you start basecalling again?
