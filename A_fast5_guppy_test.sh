f350a919a3dc9ee79875438e6545a24e65a23101
isabelluo <iisabel1234tw@gmail.com>
2022-10-06 01:23:20 -0400

    change the order of the if loop

--- A_fast5_guppy_test.sh
+++ A_fast5_guppy_test.sh
@@ -7,20 +7,22 @@ i=1
 while [ $i -le 100 ] # to call out a variable in the commands, use dollar sign
 do
     fast5files=`ls $current_directory/fast5/ | grep ".fast5" | wc -l`
-    echo $fast5files
-    if [ ! -d $current_directory/fast5/$i && $fast5files -ge 1 ]
+    if [ ! -d $current_directory/fast5/$i ]
     then
         mkdir $current_directory/fast5/$i # copy and paste your path for the current directory
-<<<<<<< HEAD
-        if [ $fast5files -ge 1 ]
-        then 
+        if [ $fast5files -ge 2 ]
+        then
             mv $current_directory/fast5/*.fast5 $current_directory/fast5/$i/.
             guppy_basecaller --input_path $current_directory/fast5/$i --save_path $current_directory/fastq/$i --flowcell FLO-MIN106 --kit SQK-LSK109 --device auto --calib_detect
             echo "${i} folder ${fast5files} files"
             ((i++))
         else
-=======
-        fast5files=`ls $current_directory/fast5/ | grep ".fast5" | wc -l`
+          sleep 30
+        fi
+    elif test -d $current_directory/fastq/$i #try to let it skip to the largest one?
+    then
+        guppy_basecaller --input_path $current_directory/fast5/$i --save_path $current_directory/fastq/$i --flowcell FLO-MIN106 --kit SQK-LSK109 --device auto --calib_detect
+        ((i++))
     elif [ $fast5files -ge 2 ] # move the files onece there are more the 2 fast5 files generated
     then
         mv $current_directory/fast5/*.fast5 $current_directory/fast5/$i/. # copy and paste your path for the current directory
@@ -28,19 +30,9 @@ do
         # the save path should be in fastq folder # specify your kit and flowcell
         echo "${i} folder ${fast5files} files " # optional,but it will tell you how many folders is in each files
         ((i++)) # increases i by 1 each time
-    elif test -d $current_directory/fastq/$i #try to let it skip to the largest one?
-    then
-        guppy_basecaller --input_path $current_directory/fast5/$i --save_path $current_directory/fastq/$i --flowcell FLO-MIN106 --kit SQK-LSK109 --device auto --calib_detect
-        ((i++))
     else
->>>>>>> f52a25fa0bd0b05d2d13ae3b0a952f793d2f0aeb
-        sleep 5
+        sleep 30
         echo "current basecalling folder ${i}"
-        fi
-    else test -d $current_directory/fastq/$i #try to let it skip to the largest one?
-        guppy_basecaller --input_path $current_directory/fast5/$i --save_path $current_directory/fastq/$i --flowcell FLO-MIN106 --kit SQK-LSK109 --device auto --calib_detect
-        echo "current basecalling folder ${i}"
-        ((i++))
     fi
 done
 # what if you turn off the window accidentally, how can you start basecalling again?
