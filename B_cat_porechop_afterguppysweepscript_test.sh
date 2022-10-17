437ec666206264b9a9582f50ce35a8e27164fa11
isabelluo <iisabel1234tw@gmail.com>
2022-10-17 17:19:19 -0400

    add comment

--- B_cat_porechop_afterguppysweepscript_test.sh
+++ B_cat_porechop_afterguppysweepscript_test.sh
@@ -5,7 +5,7 @@ while [ $i -le 100 ]
 do
     if test -d $current_directory/fastq/$i
     then
-        cd $current_directory/fastq/$i/pass/ # copy and paste your current path before i
+        cd $current_directory/fastq/$i/pass/ # I am trying to avoid changing working directories but I dont know how to do it
         cat *.fastq > ${i}_all.fastq
         porechop -i ${i}_all.fastq -b ${i}_chop_fastq \
         --check_reads 1000000 > ${i}_porechopped_output.txt
@@ -16,4 +16,3 @@ do
         sleep 10
     fi
 done
-    #try 200 seconds to begin with
