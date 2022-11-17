840a231b0ed634ff0870e2da357941c0d6daaf8c
isabelluo <iisabel1234tw@gmail.com>
2022-11-17 10:52:20 -0500

    get rid of all the cd commands

--- B_cat_porechop_afterguppysweepscript_test.sh
+++ B_cat_porechop_afterguppysweepscript_test.sh
@@ -2,17 +2,10 @@
 current_directory=$(pwd)
 i=1
 while [ $i -le 100 ]
-do
-    if test -d $current_directory/fastq/$i
-    then
-        cd $current_directory/fastq/$i/pass/ # I am trying to avoid changing working directories but I dont know how to do it
-        cat *.fastq > ${i}_all.fastq
-        porechop -i ${i}_all.fastq -b ${i}_chop_fastq \
-        --check_reads 1000000 > ${i}_porechopped_output.txt
-        echo "${i} fastq folder done"
-        ((i++))
-    else
-        echo "current fastq folder ${i}"
-        sleep 10
-    fi
+do # I am trying to avoid changing working directories but I dont know how to do it
+  cat $current_directory/fastq/$i/pass/*.fastq > ${i}_all.fastq
+  porechop -i ${i}_all.fastq -b ${i}_chop_fastq \
+  --check_reads 1000000 > ${i}_porechopped_output.txt
+  echo "${i} fastq folder done"
+  ((i++))
 done
--- D_centrifuge.sh
+++ D_centrifuge.sh
@@ -2,15 +2,17 @@
 current_directory=$(pwd)
 final_analysis=$current_directory/final_analysis
 mkdir for_BWA
+data=`ls $final_analysis | grep .fastq`
+summery=`ls $final_analysis | grep .csv`
+reads=`ls $final_analysis | grep _ReadIDs.txt`
 
-cd $final_analysis
-
-for item in *.fastq
+for item in $data
 do
     centrifuge -p 4 -k 50 -q -x vert_virusALL_08122022_GRCg7b_index -S $final_analysis/${item:0:4}_report.csv --report-file $final_analysis/${item:0:4}_report.tsv $item
 done
 awk -F'\t' '$1 ~ /Gallid alphaherpesvirus 1|herpesvirus/ {print $2}' *_report.tsv > ILTV_TaxID.txt
-for c in *.csv
+
+for c in $summery
 do
 	awk 'NR == FNR {
 		 patt[$0]; next
@@ -22,11 +24,10 @@ do
 done
 
 
-for Read in *_ReadIDs.txt
+for Read in $reads
 do
-	grep -A3 -f $Read ${Read:0:4}.fastq > $final_analysis/${Read:0:-4}.fq
-	grep -v -Fx -- -- ${Read:0:-4}.fq > $final_analysis/${Read:0:-4}.fastq
+	grep -A3 -f $Read ${Read:0:4}.fastq > $current_directory/for_BWA/${Read:0:-4}.fq
+	grep -v -Fx -- -- $current_directory/for_BWA/${Read:0:-4}.fq > $current_directory/for_BWA/${Read:0:-4}.fastq
   sequences=`grep -o "read" $final_analysis/${Read:0:-4}.fastq | wc -l`
 	echo "${Read:0:-4} $sequences" >> $current_directory/for_BWA/output_num.txt
-	cp $final_analysis/${Read:0:-4}.fastq $current_directory/for_BWA/.
 done
