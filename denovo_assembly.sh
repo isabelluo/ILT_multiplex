d6acce4d0456f1f1bf9b6babe9aaf0eff0430064
isabelluo <iisabel1234tw@gmail.com>
2022-11-21 14:39:29 -0500

    update log

--- denovo_assembly.sh
+++ denovo_assembly.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=6		                            # Number of cores per task - match this to the num_threads used by BLAST
 #SBATCH --mem=40gb			                                # Total memory for job
 #SBATCH --time=2:00:00  		                            # Time limit hrs:min:sec
-#SBATCH --output=/work/gene8940/yl87482/log.%j			    # Location of standard output and error log files (replace cbergman with your myid)
+#SBATCH --output=log.%j			    # Location of standard output and error log files (replace cbergman with your myid)
 #SBATCH --mail-user=yl87482@uga.edu                    # Where to send mail (replace cbergman with your myid)
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
