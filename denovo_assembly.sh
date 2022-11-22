46ce9ddb087124a39d179de7233d82ee60e76301
isabelluo <iisabel1234tw@gmail.com>
2022-11-22 13:28:18 -0500

    sepcify minimum read length

--- denovo_assembly.sh
+++ denovo_assembly.sh
@@ -3,9 +3,10 @@
 #SBATCH --partition=batch		                            # Partition (queue) name
 #SBATCH --ntasks=10			                                # Single task job
 #SBATCH --cpus-per-task=6		                            # Number of cores per task - match this to the num_threads used by BLAST
-#SBATCH --mem=40gb			                                # Total memory for job
+#SBATCH --mem=100gb			                                # Total memory for job
 #SBATCH --time=2:00:00  		                            # Time limit hrs:min:sec
 #SBATCH --output=log.%j			    # Location of standard output and error log files (replace cbergman with your myid)
+#SBATCH --error=err.log.%j
 #SBATCH --mail-user=yl87482@uga.edu                    # Where to send mail (replace cbergman with your myid)
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
@@ -18,7 +19,7 @@ module load canu/1.9-GCCcore-8.3.0-Java-11 MUMmer/4.0.0beta2-foss-2019b QUAST/5.
 for i in $DATA
 do
   mkdir $OUTDIR/${i:0:(-6)}.dir
-  canu -p ${i:0:(-6)} -d $OUTDIR/${i:0:(-6)}.dir genomeSize=13k useGrid=false -nanopore-corrected $REFDIR/$i
+  canu -p ${i:0:(-6)} -d $OUTDIR/${i:0:(-6)}.dir genomeSize=13k useGrid=false minReadLength=200 -nanopore-corrected $REFDIR/$i
   quast.py -o $OUTDIR/${i:0:(-6)}.dir -t 6 -r $REFDIR/24_sequence_USregion_cons.fasta $i
   nucmer $REFDIR/24_sequence_USregion_cons.fasta $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.contigs.fasta -p $OUTDIR/${i:0:(-6)}.dir/mum_canu_${i:0:(-6)}
   delta-filter -1 $OUTDIR/${i:0:(-6)}.dir/mum_canu_${i:0:(-6)}.delta > $OUTDIR/${i:0:(-6)}.dir/mum_canu_${i:0:(-6)}.1delta
