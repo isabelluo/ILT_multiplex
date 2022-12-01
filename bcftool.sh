cb38e1df5d75e265dc93cd4fe40654f7ea023b03
isabelluo <iisabel1234tw@gmail.com>
2022-12-01 13:39:14 -0500

    make consensus consistent

--- bcftool.sh
+++ bcftool.sh
@@ -22,12 +22,12 @@ do
   mkdir $OUTDIR/${i:0:(-6)}.dir
   bwa mem -M -t 8 $REFDIR/cat_US/catted_USregion.fasta $REFDIR/$i | samtools sort -o $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}_sort.bam
   samtools index $OUTDIR/${i:0:(-6)}_sort.bam # you need a index file for it to find the read counts
-  bcftools mpileup -Oz --threads 6 --min-MQ 60 -f $REFDIR/24_sequence_USregion_cons.fasta $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}_sort.bam \
+  bcftools mpileup -Oz --threads 6 --min-MQ 60 -f $REFDIR/cat_US/catted_USregion.fasta $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}_sort.bam \
   > $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.vcf.gz
   bcftools call -Oz -m -v --threads 6 --ploidy 1 $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.vcf.gz > $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.call.vcf.gz
   bcftools filter -Oz -e 'QUAL<40 || DP<10' $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.call.vcf.gz > $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.call.filter.vcf.gz
   bcftools index $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.call.vcf.gz
   bcftools index $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.call.filter.vcf.gz
-  time bcftools consensus -f $REFDIR/24_sequence_USregion_cons.fasta -o $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.call.fasta $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.call.vcf.gz
-  time bcftools consensus -f $REFDIR/24_sequence_USregion_cons.fasta -o $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.filter.fasta $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.call.filter.vcf.gz
+  time bcftools consensus -f $REFDIR/cat_US/catted_USregion.fasta -o $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.call.fasta $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.call.vcf.gz
+  time bcftools consensus -f $REFDIR/cat_US/catted_USregion.fasta -o $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.filter.fasta $OUTDIR/${i:0:(-6)}.dir/${i:0:(-6)}.mpileup.call.filter.vcf.gz
 done
