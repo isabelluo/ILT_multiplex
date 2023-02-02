03e4b79e63b0370514d6cd8aa34a746d890ce9b7
isabelluo <iisabel1234tw@gmail.com>
2023-02-02 10:04:31 -0500

    remove final analysis variable

--- D_centrifuge.sh
+++ D_centrifuge.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 current_directory=$(pwd)
-final_analysis=$current_directory/final_analysis
 mkdir for_BWA
-data=`ls $final_analysis | grep .fastq`
-summery=`ls $final_analysis | grep .csv`
-reads=`ls $final_analysis | grep _ReadIDs.txt`
+data=`ls final_analysis | grep .fastq`
+summery=`ls final_analysis | grep .csv`
+reads=`ls final_analysis | grep _ReadIDs.txt`
 
 for item in $data
 do
-  centrifuge -p 4 -k 50 -q -x vert_virusALL_08122022_GRCg7b_index -S $final_analysis/${item:0:4}_report.csv --report-file $final_analysis/${item:0:4}_report.tsv $item
-  awk -F'\t' '$1 ~ /Gallid alphaherpesvirus 1|herpesvirus/ {print $2}' $final_analysis/${item:0:4}_report.tsv >> ILTV_TaxID.txt
+  centrifuge -p 4 -k 50 -q -x vert_virusALL_08122022_GRCg7b_index -S final_analysis/${item:0:4}_report.csv --report-file final_analysis/${item:0:4}_report.tsv $item
+  awk -F'\t' '$1 ~ /Gallid alphaherpesvirus 1|herpesvirus/ {print $2}' final_analysis/${item:0:4}_report.tsv >> final_analysis/ILTV_TaxID.txt
 done
 
 
