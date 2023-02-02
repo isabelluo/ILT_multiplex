f3dd496b684bee0369972892577a9d4d74fab024
isabelluo <iisabel1234tw@gmail.com>
2023-02-02 09:46:22 -0500

    specify tsv path

--- D_centrifuge.sh
+++ D_centrifuge.sh
@@ -9,8 +9,9 @@ reads=`ls $final_analysis | grep _ReadIDs.txt`
 for item in $data
 do
     centrifuge -p 4 -k 50 -q -x vert_virusALL_08122022_GRCg7b_index -S $final_analysis/${item:0:4}_report.csv --report-file $final_analysis/${item:0:4}_report.tsv $item
+	awk -F'\t' '$1 ~ /Gallid alphaherpesvirus 1|herpesvirus/ {print $2}' $final_analysis/${item:0:4}_report.tsv >> ILTV_TaxID.txt
 done
-awk -F'\t' '$1 ~ /Gallid alphaherpesvirus 1|herpesvirus/ {print $2}' *_report.tsv > ILTV_TaxID.txt
+
 
 for c in $summery
 do
