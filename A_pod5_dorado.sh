e94a415527bdfc59a54875edc81d9a580500ba0a
isabelluo <iisabel1234tw@gmail.com>
2023-02-21 10:24:34 -0500

    flag then model

--- A_pod5_dorado.sh
+++ A_pod5_dorado.sh
@@ -13,14 +13,14 @@ QFILTER=10
 i=1
 while [ $i -le 100 ] # to call out a variable in the commands, use dollar sign
 do
-    pod5files=`ls $current_directory/fast5/ | grep ".pod5" | wc -l`
+    pod5files=`ls $current_directory/pod5/ | grep ".pod5" | wc -l`
     if [ ! -d $current_directory/pod5/$i ]  #make directory if none
     then
         mkdir $current_directory/pod5/$i # copy and paste your path for the current directory
         if [ $pod5files -ge 2 ] # move the files when there are two fast5 files, just helps me count
         then
             mv $current_directory/pod5/*.pod5 $current_directory/pod5/$i/.
-            dorado basecaller dna_r9.4.1_e8_hac@v3.3 --min_qscore 7 --emit-fastq --device cuda:auto $current_directory/pod5/$i/. > $current_directory/fastq/$i/.
+            dorado basecaller  --min-qscore 7 --emit-fastq --device cuda:auto dna_r9.4.1_e8_hac@v3.3 $current_directory/pod5/$i/. > $current_directory/fastq/$i/.
             echo "${i} folder ${pod5files} files"
             ((i++))
         else
@@ -28,12 +28,12 @@ do
         fi
     elif test -d $current_directory/fastq/$i #just in case some one close the terminal in the middle of running guppy, this line will finish the work
     then
-        dorado basecaller dna_r9.4.1_e8_hac@v3.3 --min_qscore 7 --emit-fastq --device cuda:auto $current_directory/pod5/$i/. > $current_directory/fastq/$i/.
+        dorado basecaller --min_qscore 7 --emit-fastq --device cuda:auto dna_r9.4.1_e8_hac@v3.3 $current_directory/pod5/$i/. > $current_directory/fastq/$i/.
         ((i++))
     elif [ $fast5files -ge 2 ] # move the files onece there are more the 2 fast5 files generated to preven empty folder
     then
         mv $current_directory/pod5/*.pod5 $current_directory/pod5/$i/.
-        dorado basecaller dna_r9.4.1_e8_hac@v3.3 --min_qscore 7 --emit-fastq --device cuda:auto $current_directory/pod5/$i/. > $current_directory/fastq/$i/.
+        dorado basecaller --min-qscore 7 --emit-fastq --device cuda:auto dna_r9.4.1_e8_hac@v3.3 $current_directory/pod5/$i/. > $current_directory/fastq/$i/.
         echo "${i} folder ${pod5files} files " # optional,but it will tell you how many folders is in each files
         ((i++)) # increases i by 1 each time
     else
