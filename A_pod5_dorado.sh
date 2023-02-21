7b55777acd730d1e7522e3e3ab43dc7eaff15e19
isabelluo <iisabel1234tw@gmail.com>
2023-02-21 12:01:46 -0500

    cuda:0

--- A_pod5_dorado.sh
+++ A_pod5_dorado.sh
@@ -21,20 +21,20 @@ do
         if [ $pod5files -ge 2 ] # move the files when there are two fast5 files, just helps me count
         then
             mv $current_directory/pod5/*.pod5 $current_directory/pod5/$i/.
-            dorado basecaller  --min-qscore 7 --emit-fastq --device cuda:auto dna_r9.4.1_e8_hac@v3.3 $current_directory/pod5/$i/ > $current_directory/fastq/$i/
+            dorado basecaller  --min-qscore 7 --emit-fastq --device cuda:0 dna_r9.4.1_e8_hac@v3.3 $current_directory/pod5/$i/ > $current_directory/fastq/${i}_call.fastq
             echo "${i} folder ${pod5files} files"
             ((i++))
         else
           sleep 30 # sleep if there is no files generated
         fi
-    elif test -d $current_directory/fastq/$i #just in case some one close the terminal in the middle of running guppy, this line will finish the work
+    elif test -f $current_directory/fastq/${i}_call.fastq #just in case some one close the terminal in the middle of running guppy, this line will finish the work
     then
-        dorado basecaller --min_qscore 7 --emit-fastq --device cuda:auto dna_r9.4.1_e8_hac@v3.3 $current_directory/pod5/$i/ > $current_directory/fastq/$i/
+        dorado basecaller --min_qscore 7 --emit-fastq --device cuda:0 dna_r9.4.1_e8_hac@v3.3 $current_directory/pod5/$i/ > $current_directory/fastq/$i/
         ((i++))
     elif [ $pod5files -ge 2 ] # move the files onece there are more the 2 fast5 files generated to preven empty folder
     then
         mv $current_directory/pod5/*.pod5 $current_directory/pod5/$i/.
-        dorado basecaller --min-qscore 7 --emit-fastq --device cuda:auto dna_r9.4.1_e8_hac@v3.3 $current_directory/pod5/$i/ > $current_directory/fastq/$i/
+        dorado basecaller --min-qscore 7 --emit-fastq --device cuda:0 dna_r9.4.1_e8_hac@v3.3 $current_directory/pod5/$i/ > $current_directory/fastq/${i}_call.fastq
         echo "${i} folder ${pod5files} files " # optional,but it will tell you how many folders is in each files
         ((i++)) # increases i by 1 each time
     else
